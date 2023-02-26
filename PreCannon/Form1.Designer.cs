09f03f71f545357fcf11e352a6ed1eb7bb43f421
Sergio Ramirez <sramirez@synergytpc.com>
2023-02-26 16:55:31 -0500

    Include sln into project baseline

---
+++ PreCannon/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+
+namespace PreCannon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+           
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+
+            
+    }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private async Task InitializeComponentAsync()
+        {
+            
+
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button sync;
+        private Label Time;
+        private Label PrecannonAdviser;
+        private Label Author;
+    }
+}
